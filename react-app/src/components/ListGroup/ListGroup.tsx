4f8aa2ec6b2efa8074b6aaeb82a407707f67abcc
Jakub Kowalczyk <s31559@pjwstk.edu.pl>
2025-09-10 21:02:14 +0200

    init

--- react-app/src/components/ListGroup/ListGroup.tsx
+++ react-app/src/components/ListGroup/ListGroup.tsx
@@ -1,40 +1,47 @@
 import { MouseEvent, useState } from "react";
-import styles from './ListGroup.module.css'
+import styles from "./ListGroup.css";
+import styled from "styled-components";
 
-
-interface Props {
-    items: string [];
-    heading: string;
-    // (items:string)=> void
-    onSelectItem: (item: string)=> void
+const List = styled.ul`
+  list-style: none;
+  padding: 0;
+`;
+interface ListItemProps {
+  active: boolean;
 }
 
-function ListGroup({items , heading , onSelectItem}: Props) {
+const ListItem = styled.li<ListItemProps>`
+  padding: 5px 0;
+  background: ${(props) => (props.active ? "blue" : "none")};
+`;
+interface Props {
+  items: string[];
+  heading: string;
+  // (items:string)=> void
+  onSelectItem: (item: string) => void;
+}
 
-    const [selectedIndex,setSelectedIndex]= useState(-1);
-    
-    // Event handler
-    const handleClick = (event: MouseEvent)=> console.log(event) 
-    return (
+function ListGroup({ items, heading, onSelectItem }: Props) {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  return (
     <>
-        <h1>{heading}</h1>
-        {/* { items.length === 0 ? <p>No item found</p> : null} */}
-        {  items.length === 0 && <p>No item found</p>}
-        <ul className={[styles.listGroup , styles.container].join(' ')}>
-            {items.map((item , index )=> (
-                <li 
-                    className={selectedIndex === index ? 'list-group-item active' : 'list-group-item'}
-                    key={item} 
-                    // onClick={handleClick}
-                    onClick={() => {
-                        setSelectedIndex(index);
-                        onSelectItem(item);
-                    }}
-                >
-                    {item}
-                </li>
-            ))}  
-        </ul>
+      <h1>{heading}</h1>
+      {items.length === 0 && <p>No item found</p>}
+      <List>
+        {items.map((item, index) => (
+          <ListItem
+            active={index === selectedIndex}
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
+          >
+            {item}
+          </ListItem>
+        ))}
+      </List>
     </>
   );
 }
